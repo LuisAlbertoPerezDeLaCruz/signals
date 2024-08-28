4a766b6bf7c2a9ac733562d56a47cb86565be3ba
Luis Alberto Perez <luisalbertoperez@gmail.com>
2024-08-28 12:22:48 -0400

    avance

---
+++ src/app/users/pages/users-signal-page/users-signal-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-signal-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './users-signal-page.component.html',
+  styleUrl: './users-signal-page.component.scss'
+})
+export class UsersSignalPageComponent {
+
+}
