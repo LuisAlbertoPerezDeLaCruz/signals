72c88c8b99009d5d03a4618a8ed59f8c55b020d8
Luis Alberto Perez <luisalbertoperez@gmail.com>
2024-09-10 05:14:21 -0400

    ajustes

--- src/app/users/pages/users-signal-page/users-signal-page.component.ts
+++ src/app/users/pages/users-signal-page/users-signal-page.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
+
+import { UsersService } from '../../services/users.service';
+import { filter } from 'rxjs';
+import { User } from '../../interfaces/user';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-users-signal-page',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './users-signal-page.component.html',
-  styleUrl: './users-signal-page.component.scss'
+  styleUrl: './users-signal-page.component.scss',
 })
-export class UsersSignalPageComponent {
+export class UsersSignalPageComponent implements OnInit {
+  public usersService = inject(UsersService);
 
+  public users = signal<User[]>([]);
+  public currentPage = signal(1);
+
+  public labelTotalUsers = computed(
+    () => `Total de usuarios ${this.users().length}`
+  );
+
+  // get users() {
+  //   return users;
+  // }
+
+  // get currentPage() {
+  //   return currentPage;
+  // }
+
+  // get labelTotalUsers() {
+  //   return labelTotalUsers;
+  // }
+
+  ngOnInit(): void {
+    this.loadPage(this.currentPage());
+  }
+
+  loadPage(page: number) {
+    this.usersService
+      .loadPage(page)
+      .pipe(filter((users) => users.length > 0))
+      .subscribe((newUsers) => {
+        this.currentPage.set(page);
+        // this.users.set( users );
+        // this.users.set([ ...this.users(), ...users ]);
+        this.users.update((currentUsers) => [...currentUsers, ...newUsers]);
+      });
+  }
 }
